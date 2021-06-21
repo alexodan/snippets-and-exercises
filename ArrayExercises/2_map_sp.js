2dc2837a78ab17b9b57528678a5acc662b69108a
Alexis Diaz <alexdan1993@gmail.com>
2021-06-21 19:56:16 -0300

    Translations

--- ArrayExercises/2_map_sp.js
+++ ArrayExercises/2_map_sp.js
@@ -7,3 +7,10 @@
 // e.g. valuesTimeIndex([1, 2, 3, 4]) => [0, 2, 6, 12]
 // e.g. valuesTimeIndex([1, -2, -5, 10]) => [0, -2, -10, 30]
 
+
+// 3) ☠️️ Escribir una funcion llamada 'extractKey' que acepte un array de objetos y una clave (key) y retorne un nuevo array cuyos valores sean el valor correspondiente a la clave de cada objeto
+// e.g. extractKey([{name: 'Gillian'}, {name: 'Paula'}, {name: 'Sebastian'}], 'name') => ['Gillian', 'Paula', 'Sebastian']
+
+
+// 4) ☠️️☠️️ Escribir una funcion llamada 'extractFullName' que acepte un array de objetos y retorne un nuevo array cuyos valores sean los valores de las keys 'first' y 'last' concatenadas.
+// e.g. extractFullName([{first: 'Martin', last:"Del Potro"}, {first: 'Luciana', last:"Aymar"}]) => ['Martin Del Potro', 'Luciana Aymar']
