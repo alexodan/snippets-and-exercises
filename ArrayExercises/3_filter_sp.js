dd1691821dbc2d9876c89724457bdee4a4cb5314
alexodan <alexdan1993@gmail.com>
2021-06-22 20:57:52 -0300

    Filter exercises

---
+++ ArrayExercises/3_filter_sp.js
@@ -0,0 +1,19 @@
+// 1) ☠️: Escribe una funcion llamada 'filterByValue' que acepte un array de objetos y una key, y retorne un nuevo array con todos los objetos que contengan esa key.
+// e.g. filterByValue([{a: 'foo'}, {b: '000'}, {a: 'bar'}, {c: 'nnn}], 'a') => [{a: 'foo'}, {a: 'bar'}]
+
+
+// 2) ☠️  Escribe una funcion llamada 'find' que acepte un array y un valor X y retorne el primer elemento en el array que tenga el mismo valor que X o undefined si el valor no se encuentra en el array.
+// e.g. find([1, 2, 3, 4, 5], 3) => 3
+// e.g. find(['a', 'b', 'c'], 'k') => undefined
+
+
+// 3) ☠️ ☠️  Escribe una funcion llamada 'removeVowels' que acepte un string y retorne un nuevo string con todas las vocales removidas.
+// e.g. removeVowels('murcielago') => 'mrclg'
+
+
+// 4) ☠️ ☠️  Escribe una funcion llamada 'doubleEvenNumbers' que acepte un array y retorne un nuevo array con solo los numeros pares duplicados.
+// e.g. doubleEvenNumbers([1, 2, 3, 4, 5, 6]) => [4, 8, 12]
+// e.g. doubleEvenNumbers([1, 3, 5]) => []
+// Pensa que otro metodo visto anteriormente puede ayudarte a hacer esto.
+// Da lo mismo primero usar uno y luego otro? O seria mas eficiente usarlos en un determinado orden?
+
